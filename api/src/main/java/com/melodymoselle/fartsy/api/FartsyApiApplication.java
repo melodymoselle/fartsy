b3a16d2b9a455607b86da5c49c141e2db963ff54
Melmo <melodymoselle@gmail.com>
2017-12-23 21:08:39 -0800

    Project setup

---
+++ api/src/main/java/com/melodymoselle/fartsy/api/FartsyApiApplication.java
@@ -0,0 +1,12 @@
+package com.melodymoselle.fartsy.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FartsyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FartsyApiApplication.class, args);
+	}
+}
